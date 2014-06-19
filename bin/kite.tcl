010feb608ea3ae728454c034b3e1c113359f5fe6
Will Duquette <William.H.Duquette@jpl.nasa.gov>
2014-06-19 12:35:15 -0700

    Added skeleton main program
    bin/kite.tcl is the Kite loader module; it will ultimately load app code
    from lib/*.  build.sh is a temporary script to build kite.kit, which is
    the standalone tool.

---
+++ bin/kite.tcl
@@ -0,0 +1,79 @@
+#!/bin/sh
+# -*-tcl-*-
+# The next line restarts using tclsh \
+exec tclsh "$0" "$@"
+
+#-----------------------------------------------------------------------
+# TITLE:
+#    kite.tcl
+#
+# AUTHOR:
+#    Will Duquette
+#
+# DESCRIPTION:
+#    Kite: Application Launcher
+#
+#    This script serves as the main entry point for the Kite
+#    tool.  The tool is invoked using 
+#    the following syntax:
+#
+#        $ kite ?args....?
+#
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# Set up the auto_path, so that we can find the correct libraries.  
+# In development, there might be directories loaded from TCLLIBPATH;
+# strip them out.
+
+# First, remove all TCLLIBPATH directories from the auto_path.
+
+if {[info exists env(TCLLIBPATH)]} {
+    set old_path $auto_path
+    set auto_path [list]
+
+    foreach dir $old_path {
+        if {$dir ni $env(TCLLIBPATH)} {
+            lappend auto_path $dir
+        }
+    }
+}
+
+# Next, get the Kite-specific library directories.  Whether we're
+# in a starkit or not, the libraries can be found relative to this
+# script file.
+
+set appdir  [file normalize [file dirname [info script]]]
+set libdir  [file normalize [file join $appdir .. lib]]
+
+# Add Kite libs to the new lib path.
+lappend auto_path $libdir
+
+#-------------------------------------------------------------------
+# Next, require Tcl/Tk
+
+package require Tcl 8.6
+
+
+#-----------------------------------------------------------------------
+# Main Program 
+
+# main argv
+#
+# argv       Command line arguments
+#
+# This is the main program; it is invoked at the bottom of the file.
+# It determines the application to invoke, and does so.
+
+proc main {argv} {
+    puts "Hello, <$argv>!"
+}
+
+
+#-----------------------------------------------------------------------
+# Run the program
+
+# FIRST, run the main routine, to set everything up.
+main $argv
+
+
