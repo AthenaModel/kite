e7a893b65db4acc6c5c6d06df2f364362a13e606
Will Duquette <William.H.Duquette@jpl.nasa.gov>
2014-08-27 13:39:26 -0700

    Bumped version to 0.1.5a0
    Preparing for future development.

--- lib/kiteutils/pkgModules.tcl
+++ lib/kiteutils/pkgModules.tcl
@@ -14,7 +14,7 @@
 # Package Definition
 
 # -kite-provide-start  DO NOT EDIT THIS BLOCK BY HAND
-package provide kiteutils 0.1.4
+package provide kiteutils 0.1.5a0
 # -kite-provide-end
 
 #-----------------------------------------------------------------------
