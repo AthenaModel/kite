1c3da7804feacfbfbfbc6318c39bc5cfba148e67
Will Duquette <William.H.Duquette@jpl.nasa.gov>
2014-10-21 09:24:27 -0700

    Bumping version to 0.4.4a0
    This version will be in development for a while, so bumping version to
    "a0".

--- lib/kiteinfo/kiteinfo.tcl
+++ lib/kiteinfo/kiteinfo.tcl
@@ -58,7 +58,7 @@ namespace eval ::kiteinfo:: {
         apptype-kite exe
         local-textutil::expander 0
         local-snit 0
-        version 0.4.3
+        version 0.4.4a0
         reqver-crc32 1.3
         dists install-%platform
     }
--- lib/kiteutils/pkgIndex.tcl
+++ lib/kiteutils/pkgIndex.tcl
@@ -9,7 +9,7 @@
 #   Kite: kiteutils package index.
 
 # -kite-ifneeded-start  DO NOT EDIT THIS BLOCK BY HAND
-package ifneeded kiteutils 0.4.3 [list source [file join $dir pkgModules.tcl]]
+package ifneeded kiteutils 0.4.4a0 [list source [file join $dir pkgModules.tcl]]
 # -kite-ifneeded-end
 
 
